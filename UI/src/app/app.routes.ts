7f3d98e802015ec4087d20d3d3fc2eba5ab689bc
fpata <fakhruddin.pata@gmail.com>
2025-08-18 16:50:52 +0530

    bug fix

--- UI/src/app/app.routes.ts
+++ UI/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './componets/login/login.component';
 import { PatientMasterComponent } from './componets/patient/patient-master/patient-master.component';
 import { SchedulerComponent } from './common/scheduler/scheduler';
-import { Dashboard } from './componets/dashboard/dashboard.component';
+import { DashboardComponent } from './componets/dashboard/dashboard.component';
 import { DoctorAppointmentsComponent } from './componets/doctor/doctorappointments/doctorappointments.component';
 import { PatientCompleteHistoryComponent } from './componets/patient/patientcompletehistory/patient-complete-history.component';
 import { UserMasterComponent } from './componets/user/user-master/user-master.component';
@@ -14,7 +14,7 @@ export const routes: Routes = [
   { path: 'patient', component: PatientMasterComponent, canActivate: [authGuard] },
   { path: 'scheduler', component: SchedulerComponent, canActivate: [authGuard] },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
   { path: 'doctorAppointments', component: DoctorAppointmentsComponent, canActivate: [authGuard] },
   { path : 'patienthistory', component: PatientCompleteHistoryComponent, canActivate: [authGuard] },
   { path: 'user', component : UserMasterComponent, canActivate: [authGuard] }
